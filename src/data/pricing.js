1ec524e6c0c27ede0c994ba3dc6d8fd64202d99b
Robin De Wolf <rdewolf@telenet.be>
2026-04-21 14:59:36 +0200

    updates

--- src/data/pricing.js
+++ src/data/pricing.js
@@ -1,11 +1,12 @@
 export const pricing = {
   diagnosisHome: 40,
-  pcCleanupHome: 120,
-  pcCleanupPickup: 100,
-  windowsReinstallPickup: 120,
+  simplePcCleanupHome: 100,
+  simplePcCleanupPickup: 80,
+  deepPcCleanupHome: 150,
+  deepPcCleanupPickup: 120,
   advancedHourly: 75,
   advancedMinimumHours: 2,
   pickupDropoffWithinKm: 20,
   pickupDropoffFee: 25,
-  pickupDropoffFreeFrom: 150
+  pickupDropoffFreeFrom: 120
 };
