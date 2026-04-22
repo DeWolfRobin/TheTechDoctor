551b4611b64d017269d20a49c53f1430022bc5b6
Robin De Wolf <rdewolf@telenet.be>
2026-04-22 16:10:44 +0200

    cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 
-const repository = process.env.GITHUB_REPOSITORY ?? "";
-const [owner, repo] = repository.split("/");
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const isUserOrOrgPagesRepo = owner && repo && repo.toLowerCase() === `${owner.toLowerCase()}.github.io`;
-
-const base = isGithubActions && !isUserOrOrgPagesRepo && repo ? `/${repo}/` : "/";
-const site = owner ? `https://${owner}.github.io${isUserOrOrgPagesRepo ? "/" : `/${repo}/`}` : undefined;
+const site = "https://techdoctor.be";
 
 export default defineConfig({
   site,
-  base,
+  base: "/",
   server: {
     port: 4321,
     host: true
